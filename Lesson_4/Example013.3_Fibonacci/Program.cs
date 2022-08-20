f2b6db4d6212f644d290c9d100561621c23d6d8e
Suxarik777 <gultcev@mail.ru>
2022-08-20 17:42:07 +0300

    двумерный массив, рекурсия, метод заполнения рисунка

---
+++ Lesson_4/Example013.3_Fibonacci/Program.cs
@@ -0,0 +1,51 @@
+﻿//Фибоначчи - каждое следующее число задаётся через предыдущее
+//1 1 2 3 5 8 13 21
+
+
+// //просто цикл
+// int n = 10946;
+// int fibon1 = 0;
+// int fibon2 = 1;
+// Console.Write("0 1" + " ");
+// while (fibon2 <= n - fibon1)
+// {
+// int temp = fibon2;
+// fibon2 = fibon2 + fibon1;
+// fibon1 = temp;
+// Console.Write(fibon2 + " ");
+// }
+
+// ///////////////////////////////
+
+// //через массив
+// int[] fibonacci = new int[1000];
+// fibonacci[0] = 1;
+// fibonacci[1] = 1;
+// int n = int.Parse(Console.ReadLine() ?? "0");
+// Console.Write($"{fibonacci[0]} {fibonacci[1]} ");
+// int index = 1;
+// while(fibonacci[index]<n)
+// {
+//     index++;
+//     fibonacci[index]=fibonacci[index-1]+fibonacci[index-2];
+//     if(fibonacci[index]<n)
+//         Console.Write($"{fibonacci[index]} ");
+// }
+
+/////////////////////////
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n - 2);
+}
+
+int numberFibo = 10;
+for(int i = 1; i < numberFibo; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
---
+++ Lesson_4/Example013_dvumerni_massiv_Print_Fill/Program.cs
@@ -0,0 +1,55 @@
+﻿// создаём двумерный массив
+// [строки, столбцы]
+
+string[,] table = new string [2,5]; //создание элемента мы указываем КОЛИЧЕСТВО!!!!!
+                                    // как команда .Length
+                                    // а вот индекс будет от 0
+// расписываем
+//                 столбец[i=0]    столбец[i=1]    столбец[i=2]    столбец[i=3]    столбец[i=4]     
+//
+//строка[i=0]  //  table [0,0]     table [0,1]     table [0,2]     table [0,3]     table [0,4]
+//строка[i=1]      table [1,0]     table [1,1]     table [1,2]     table [1,3]     table [1,4]
+
+
+table[1,2] = "слово"; //обращение к [первой строке, 2 столбцу] - в данном случае записали "слово"
+
+for(int rows = 0; rows < 2; rows++) //цикл по строками
+{
+    for(int columns = 0; columns < 5; columns++) //цикл по столбцам
+    {
+        Console.WriteLine("-" + table[rows, columns] + "-");
+    }
+}
+
+////////////////////////////////////////
+
+void PrintArray(int[,] matrix)
+{
+    for(int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for(int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            Console.Write($"{matrix[rows, columns]} "); // выводим столбцы в одну строку
+        }
+    Console.WriteLine(); //после того, как внутренним циклом пробежались по столбцам, на новую строку
+    }      
+}
+
+void FillArray(int[,] matrix)
+{
+    var random = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
